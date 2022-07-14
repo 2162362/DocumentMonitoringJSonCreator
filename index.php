7a89794601d07e0f716f04934a6cb57b626a81a3
Mihail Stratan <2162362@my.ipleiria.pt>
2022-07-14 20:04:48 +0100

    - added some code

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_POST['importfile'])){
+        // read json file
+        $data = file_get_contents($_FILES['importfile']['tmp_name']);
+        $_SESSION['data'] = $data;
+     }
+?>
+<!DOCTYPE html>
+<body>
+    <form method="POST">
+        <input type="file" name="importfile" id="inportfile" onchange="this.form.submit()" accept="application/json" style="display:none"/> 
+        <button id="import">Import</button>
+        <script>
+        $('#import').click(function(){ $('#inportfile').trigger('click'); });
+        </script>
+    </form>
+
+</body>
