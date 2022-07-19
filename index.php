10366c54b27a2e00b94dd8f7e03c8c125859a5fa
Mihail Stratan <2162362@my.ipleiria.pt>
2022-07-19 21:26:56 +0100

    - working file upload

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 <?php
-    if(isset($_POST['importfile'])){
-        // read json file
-        $data = file_get_contents($_FILES['importfile']['tmp_name']);
-        $_SESSION['data'] = $data;
-     }
+   if(isset($_FILES['json_file'])){
+      $errors= array();
+      $file_name = $_FILES['json_file']['name'];
+      $file_size = $_FILES['json_file']['size'];
+      $file_tmp = $_FILES['json_file']['tmp_name'];
+      $file_type = $_FILES['json_file']['type'];
+      $file_ext=pathinfo($_FILES['json_file']['name'], PATHINFO_EXTENSION);
+      //pathinfo($file_name, PATHINFO_EXTENSION)
+      
+      $extensions= array("json");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JSON file.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be exactly 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         //move_uploaded_file($file_tmp,"files/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
 ?>
-<!DOCTYPE html>
-<body>
-    <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="importfile" id="inportfile" onchange="this.form.submit()" accept="application/json" style="display:none"/> 
-        <button id="import">Import</button>
-        <script>
-        $('#import').click(function(){ $('#inportfile').trigger('click'); });
-        </script>
-    </form>
-    <script src="index.js" />
-</body>
+<html>
+   <body>
+      
+      <form action = "" method = "POST" enctype = "multipart/form-data">
+         <input type = "file" name = "json_file" />
+         <input type = "submit"/>
+		<?php
+            if(isset($_FILES['json_file'])){
+            
+                echo'    <ul>
+                    <li>Sent file: '. $_FILES['json_file']['name'] .'
+                    <li>File size: '. $_FILES['json_file']['size'] .'
+                    <li>File type: '. $_FILES['json_file']['type'] .'
+                    </ul>';
+            }
+        ?>
+			
+      </form>
+      
+   </body>
+</html>
